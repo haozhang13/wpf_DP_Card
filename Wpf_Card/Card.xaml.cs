c8731ebacc0fc75dde7ba52c9026f96872d3a307
haozhang13 <haozhang13@xbangs>
2019-09-06 16:43:07 +0800

    ss

---
+++ Wpf_Card/Card.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Card
+{
+    /// <summary>
+    /// Caer.xaml 的交互逻辑
+    /// </summary>
+    public partial class Card : UserControl
+    {
+        public Card()
+        {
+            InitializeComponent();
+        }
+
+        public static string[] Suits = { "Club", "Diamond", "Heart", "Spade" };
+
+
+       //Get获取依赖属性值，Set设置依赖属性值
+        public string Suit
+        {
+            get { return (string)GetValue(SuitProperty); }
+            set { SetValue(SuitProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Suit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SuitProperty =
+            DependencyProperty.Register("Suit", typeof(string), typeof(Card),
+                new PropertyMetadata("Club", new PropertyChangedCallback(OnSuitChanged)),
+                new ValidateValueCallback(ValidateSuit));
+
+
+
+        public int Rank
+        {
+            get { return (int)GetValue(RankProperty); }
+            set { SetValue(RankProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Rank.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RankProperty =
+            DependencyProperty.Register("Rank", typeof(int), typeof(Card), new PropertyMetadata(1),
+                                        new ValidateValueCallback(ValidateRank));
+
+        //是否有效
+        private static bool ValidateSuit(object value)
+        {
+            string suitValueString = (string)value;
+
+            if(suitValueString != "Club" && suitValueString !="Diamond"
+                &&suitValueString != "Heart" && suitValueString !="Spade")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool ValidateRank(object value)
+        {
+            int rankValueInt = (int)value;
+            if(rankValueInt < 1 || rankValueInt > 12)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static void OnSuitChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Card)d).SetTextColor();
+        }
+
+        private void SetTextColor()
+        {
+            if(Suit == "Club" || Suit == "Spade")
+            {
+                RankLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                SuitLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                RanklabelInverted.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            }
+            else
+            {
+                RankLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                SuitLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                RanklabelInverted.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            }
+        }
+    }
+}
--- Wpf_Card/MainWindow.xaml.cs
+++ Wpf_Card/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace Wpf_Card
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Card Card;
+        private Point offset;
+        private Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ContentGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.Source is Card)
+            {
+                Card = (Card)e.Source;
+                offset = Mouse.GetPosition(Card);
+                contentGrid.Children.Remove(Card);
+            }
+            else
+            {
+                Card = new Card
+                {
+                    Suit = Card.Suits[random.Next(0, 4)],
+                    Rank = random.Next(1, 13)
+                };
+
+                Card.HorizontalAlignment = HorizontalAlignment.Left;
+                Card.VerticalAlignment = VerticalAlignment.Top;
+                offset = new Point(50, 75);
+            }
+
+            contentGrid.Children.Add(Card);
+            PositionCard();
+        }
+
+        private void PositionCard()
+        {
+            Point pt = Mouse.GetPosition(this);
+            Card.Margin = new Thickness(pt.X - offset.X, pt.Y - offset.Y, 0, 0);
+        }
+
+        private void ContentGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Card = null;
+        }
+
+        private void ContentGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(Card != null)
+            {
+                PositionCard();
+            }
         }
     }
 }
